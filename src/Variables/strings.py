ab5a61a8d2a4ba36c9a0b331481ff897cdae9080
Ethan McWilliams <ethan.mcwilliams@majunga.co.uk>
2019-12-18 17:27:25 +0000

    Added launch json and first bit of revision

---
+++ src/Variables/strings.py
@@ -0,0 +1,7 @@
+# Variables are how we stored information, such as text, numbers or lists
+# You can also look at variables like a Label for something
+this_Is_A_Variable = "that is a string"
+
+# We can then use print to display variable in the console window
+print("To print the variable to the console you can do the following")
+print(this_Is_A_Variable)
